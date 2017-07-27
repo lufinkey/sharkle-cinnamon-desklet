885feefd4fa88b1eaaf79e568fb203334ba87bfd
lufinkey <luisfinke@gmail.com>
2017-07-27 16:35:51 -0400

    animation

--- sharkle@lufinkey/desklet.js
+++ sharkle@lufinkey/desklet.js
@@ -2,12 +2,96 @@
 const Desklet = imports.ui.desklet;
 const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+
+
+
+function Animation(fps)
+{
+	this._init(fps);
+}
+Animation.prototype = {
+	_init: function(fps)
+	{
+		this.images = [];
+		this.fps = fps;
+	},
+	
+	addImage: function(image)
+	{
+		this.images.push(image);
+	},
+}
+
+
+
+
+function AnimationPlayer()
+{
+	this._init();
+}
+AnimationPlayer.prototype = {
+	_init: function(){
+		this.loop_id = 0;
+		this.animation = null;
+		this.frame = 0;
+		this.onUpdate = null;
+	},
+	
+	setAnimation: function(animation)
+	{
+		if(this.animation!=null)
+		{
+			Mainloop.source_remove(this.loop_id);
+			this.loop_id = 0;
+		}
+		this.frame = 0;
+		this.animation = animation;
+		if(this.animation!=null)
+		{
+			this.loop_id = Mainloop.timeout_add(1000.0/this.animation.fps, Lang.bind(this, this._update_loop));
+		}
+		if(this.onUpdate!=null)
+		{
+			this.onUpdate();
+		}
+	},
+	
+	getCurrentImage: function()
+	{
+		if(this.animation==null)
+		{
+			return null;
+		}
+		else if(this.animation.images.length > 0)
+		{
+			return this.animation.images[this.frame];
+		}
+		return null;
+	},
+	
+	_update_loop: function()
+	{
+		this.frame++;
+		if(this.frame >= this.animation.images.length)
+		{
+			this.frame = 0;
+		}
+		this.loop_id = Mainloop.timeout_add(1000.0/this.animation.fps, Lang.bind(this, this._update_loop));
+		if(this.onUpdate!=null)
+		{
+			this.onUpdate();
+		}
+	},
+}
+
+
 
 function Sharkle(metadata, desklet_id)
 {
-	this._init(metadata, desklet_id)
+	this._init(metadata, desklet_id);
 }
-
 Sharkle.prototype = {
 	__proto__: Desklet.Desklet.prototype,
 
@@ -23,28 +107,47 @@ Sharkle.prototype = {
 		this.mainContent = new Clutter.Group();
 		this.mainContent.set_size(this.width, this.height);
 		this.setContent(this.mainContent);
-
-		// Setup the sharkle image
-		this.mainImageFrame = new St.Bin();
-		this.mainImageFrame.set_size(this.width, this.height);
-		this.mainImageFrame.depth = 0.5;
-		this.sharkleImage = this.loadImage("images/white/hello_0.png");
-		this.mainImageFrame.add_actor(this.sharkleImage);
 		
-		this.mainContent.add_actor(this.mainImageFrame);
+		//load animations
+		this.idleAnimation = new Animation(8);
+		for(var i=0; i<=7; i++)
+		{
+			this.idleAnimation.addImage(this.loadImage("images/white/idle_"+i+".png"));
+		}
+		
+		
+
+		// Setup the shark
+		var shark = new St.Bin();
+		shark.set_size(this.width, this.height);
+		
+		var lastSharkActor = null;
+		var sharkPlayer = new AnimationPlayer();
+		sharkPlayer.onUpdate = function(){
+			if(lastSharkActor!=null)
+			{
+				shark.remove_actor(lastSharkActor);
+			}
+			lastSharkActor = sharkPlayer.getCurrentImage();
+			if(lastSharkActor!=null)
+			{
+				shark.add_actor(lastSharkActor);
+			}
+		};
+		sharkPlayer.setAnimation(this.idleAnimation);
+		
+		this.shark = shark;
+		this.sharkPlayer = sharkPlayer;
+		
+		this.mainContent.add_actor(this.shark);
 		
 		//Setup the word bubble
 		this.wordBubble = new St.Bin();
 		this.wordBubble.set_size(this.width, this.height);
 		this.wordBubble.anchor_x = (this.width*0.63);
 		this.wordBubble.anchor_y = (this.height*0.63);
-		this.wordBubble.depth = 0.5;
-		this.bubbleImage = this.loadImage("images/white/bubble_0.png");
-		this.wordBubble.add_actor(this.bubbleImage);
 		
 		this.mainContent.add_actor(this.wordBubble);
-		
-		this.mainContent.show_all();
 	},
 
 	loadImage: function(filePath) {
