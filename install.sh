7efe63a518aa3f4f197c184105ad2d9d1f16b371
Luis Finke <luisfinke@gmail.com>
2017-07-31 14:19:56 -0400

    Make desklets folder in case it doesn't exist

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ base_dir=$(dirname "${BASH_SOURCE[0]}")
 cd "$base_dir"
 
 rm -rf ~/.local/share/cinnamon/desklets/sharkle@lufinkey
+mkdir -p ~/.local/share/cinnamon/desklets
 cp -r sharkle@lufinkey ~/.local/share/cinnamon/desklets/sharkle@lufinkey
